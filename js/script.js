6027ad2519ab06fac2e994fffde8ee95e658a127
AlessiaOsman <alessia.osman@gmail.com>
2023-12-18 16:27:41 +0100

    Analisi

--- js/script.js
+++ js/script.js
@@ -1,2 +1,9 @@
 // Controllo
 console.log('JS OK')
+
+// Chiedere km da percorrere
+// Chiedere età utente
+// Calcolare il prezzo a km per i km da percorrere
+// Applicare sconto del 20% ai minori di 18
+// Applicare scontro del 40% agli over 65
+// Output prezzo finale con massimo due decimali
