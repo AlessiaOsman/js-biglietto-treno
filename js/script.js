736b87d065617ab9359f5c91e410dc27a1815095
AlessiaOsman <alessia.osman@gmail.com>
2023-12-18 16:36:12 +0100

    added km and age variable

--- js/script.js
+++ js/script.js
@@ -7,3 +7,11 @@ console.log('JS OK')
 // Applicare sconto del 20% ai minori di 18
 // Applicare scontro del 40% agli over 65
 // Output prezzo finale con massimo due decimali
+
+const numberKm = parseInt(prompt('Quanti km devi percorrere?', 20))
+const userAge = parseInt(prompt('Qual è la tua età?', 39))
+
+console.log(numberKm, userAge)
+
+const ticketPrice = numberKm*0.21
+console.log(ticketPrice)
